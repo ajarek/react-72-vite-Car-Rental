683cd329e8bee877a8c67c9a7e4710b8f209f9b3
Ajarek <ajarek@poczta.onet.pl>
2023-04-05 00:48:34 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+     <img src="/images/car-1.png" alt="" />
+     <img src="/images/car-2.png" alt="" />
+     <img src="/images/car-3.png" alt="" />
+     <img src="/images/car-4.png" alt="" />
+    </div>
+  )
+}
+
+export default App
